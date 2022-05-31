a1dc337f0a5de86912f89d9150f9c1d97eacbb99
Ahmed Mohamed Yousef <a41155098@gmail.com>
2022-05-31 14:45:13 +0200

    Creating Upcoming TableView inside Upcoming Tab

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    private var titles: [Title] = []
+    private let upcomingTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .never
+        
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
     }
-    */
+    
+    
+    private func fetchData() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+                
+            case .success(let title):
+                self?.titles = title
+                DispatchQueue.main.async { self?.upcomingTable.reloadData() }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 
+}
+
+
+extension UpcomingViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unkown"
+        return cell
+    }
+    
+    
 }
